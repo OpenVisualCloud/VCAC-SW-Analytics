54b1ed8f1cdc8fab595cd068161f6ab59d03982d
yuyinzhx <yuyinzhx@intel.com>
2020-02-29 21:41:29 -0500

    creat readme

--- apps/src/version.cpp
+++
@@ -1,6 +0,0 @@
-#include "version.h"
-
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x)
-
-const char BUILD_VERSION[] = "build: " STR(BUILDNO) " build on: " STR(BUILDDATE);
--- buildscripts/dracut_module/50mountloopdev/mount_loop_device.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-modprobe loop
-losetup /dev/loop0 /root/root_partition.img
